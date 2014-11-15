4261c74bedba5a7e20377d7cbce409488453fd9f
Szu-Kai Hsu (brucehsu) <brucehsu@gmail.com>
2014-11-16 02:15:54 +0800

    reorder confgs

--- config.rb
+++ config.rb
@@ -1,6 +1,11 @@
-KEYWORDS = %W[compiler, llvm, gcc, vm, javascript, performance]
-HASHTAGS = %W[garbagebot56]
 FACEBOOK_APP_KEY = ''
 FACEBOOK_APP_SECRET = ''
 FACEBOOK_ACCESS_TOKEN = ''
-RSS_FEEDS = %W[http://slashdot.org/index.rdf]
+
+HASHTAGS = %W[garbagebot56]
+KEYWORDS = %W[compiler, llvm, clang, gcc, vm, javascript, performance, raptor, ios, mac, ruby, rust, rubinius, iphone]
+RSS_FEEDS = ["http://rss.slashdot.org/Slashdot/slashdot",
+             "http://www.phoronix.com/rss.php",
+             "http://www.osnews.com/files/recent.xml",
+             "http://www.rubyinside.com/feed/",
+             "http://feeds.feedburner.com/TechCrunch/"]
