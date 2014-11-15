6b978fe6502c2b8a7dda389232fb7cce8c2de698
Szu-Kai Hsu (brucehsu) <brucehsu@gmail.com>
2014-11-16 02:44:34 +0800

    Run cron jobs via whenver

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,4 @@
+set :output, ""
 set :path, "YOUR_BOT_PATH"
 set :rbenv_path, "YOUR_RBENV_PATH"
 
@@ -5,5 +6,5 @@ job_type :rbenv_rake, %Q{export PATH=:rbenv_path/shims::rbenv_path/rbenv/bin:/us
                          cd :path && bundle exec rake :task --silent :output }
 
 every 1.days do
-  # TODO
+  rbenv_rake 'run'
 end
