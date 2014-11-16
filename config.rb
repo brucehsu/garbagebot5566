95f66ccbd2a0ef30490d8a947df6a63366b34a5a
Szu-Kai Hsu (brucehsu) <brucehsu@gmail.com>
2014-11-16 13:25:34 +0800

    update keywords

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ FACEBOOK_APP_SECRET = ''
 FACEBOOK_ACCESS_TOKEN = ''
 
 HASHTAGS = %W[garbagebot56]
-KEYWORDS = %W[compiler llvm clang gcc vm javascript performance raptor ios mac ruby rails rust rubinius iphone dart qt java git github, c++ cloud dropbox amazon database aws mozilla osx yosemite retina hhvm pypy pyston jit lisp hippy]
+KEYWORDS = %W[compiler llvm clang gcc vm javascript performance raptor ios mac ruby rails rust rubinius iphone dart qt java git github, c++ cloud dropbox amazon database aws mozilla osx yosemite retina hhvm pypy pyston jit lisp hippy browser kernel os concurrency framework just-in-time qemu emacs swift scala benchmark]
 RSS_FEEDS = ["http://rss.slashdot.org/Slashdot/slashdot",
              "http://www.phoronix.com/rss.php",
              "http://www.osnews.com/files/recent.xml",
